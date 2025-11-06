a0f5f2a5571947833ea548032525479e35824b3d
ansar-a17 <ansardx@gmail.com>
2025-11-06 03:46:21 +0100

    text-to-speech API complete

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,6 +1,24 @@
 from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 import torch
 import soundfile as sf
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+import uvicorn
+from datetime import datetime, timezone
+from time import strftime
+import os
+
+app = FastAPI()
+
+class Text(BaseModel):
+    text: str
+
+class SpeechOut(BaseModel):
+    message: str
+    file_path: str
+
+PATH = "./audio_files"
 
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
@@ -8,10 +26,43 @@ vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
 speaker_embeddings = torch.randn(1, 512) * 0.1
 
-def text_to_speech(text):
-    inputs = processor(text=text, return_tensors="pt")
 
+def clean_up(n=10):
+    files = os.listdir(PATH)
+    audio_files = []
+
+    for file in files:
+        if file.split('.')[1] == "wav":
+            audio_files.append(file)
+
+    audio_files.sort()
+
+    for file_to_delete in audio_files[:-n]:
+        os.remove(f"{PATH}/{file_to_delete}")
+
+
+@app.post("/text_to_speech")
+async def text_to_speech(text: Text):
+    inputs = processor(text=text.text, return_tensors="pt")
     speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+    
+    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
 
-    sf.write("output_speech.wav", speech.numpy(), samplerate=16000)
-    print("Audio saved to output_speech.wav")
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
+    if len(os.listdir(PATH)) > 9:
+        clean_up()
+
+    output_path = f"./audio_files/output_speech_{current_time}.wav"
+    sf.write(output_path, speech.numpy(), samplerate=16000)
+    print(f"Audio saved to {output_path}")
+    
+    return FileResponse(output_path, media_type="audio/wav", filename="speech.wav")
+
+@app.get("/")
+async def home():
+    return {"message": "running!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=7999)
