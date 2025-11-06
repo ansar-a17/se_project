ad1a43236f27684830a8fde6553d51f701180f42
ansar-a17 <ansardx@gmail.com>
2025-11-06 18:17:45 +0100

    image_to_text API complete

--- image_to_text.py
+++ image_to_text.py
@@ -1,9 +1,23 @@
 from PIL import Image
 from transformers import pipeline
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import uvicorn
+import io
 
-def image_to_text(image: Image):
-    pipe = pipeline("image-to-text", model="Salesforce/blip2-opt-2.7b")
+app = FastAPI()
 
+class TextOut(BaseModel):
+    text: str
+
+pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+
+@app.post("/image_to_text", response_model=TextOut)
+async def image_to_text(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    
     result = pipe(image)
 
     if isinstance(result, list):
@@ -20,5 +34,12 @@ def image_to_text(image: Image):
 
     if text:
         text = text.strip()
-        
-    return text
+    
+    return TextOut(text=text)
+
+@app.get("/")
+async def home():
+    return {"message": "running!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
